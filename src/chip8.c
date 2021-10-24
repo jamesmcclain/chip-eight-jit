67c6f9048a18e2ca1b77b0ddea5a0e37bcec8beb
James W. McClain <jmcclain@daystrom-data-concepts.com>
2021-10-23 20:33:31 -0400

    State

--- src/chip8.c
+++ src/chip8.c
@@ -1,3 +1,13 @@
 #include <stdint.h>
 #include "chip8.h"
 
+uint16_t stack[0x10];
+uint8_t __attribute__((aligned(0x1000))) display[128*64];
+uint8_t __attribute__((aligned(0x1000))) memory[0x1000];
+uint8_t regs[16];
+
+int16_t delay_timer = 0;
+int16_t sound_timer = 0;
+int8_t stack_pointer = 0;
+uint16_t addr = 0;
+uint16_t program_counter = 0;
--- src/chip8.h
+++ src/chip8.h
@@ -3,6 +3,15 @@
 #ifndef __CHIP8__
 #define __CHIP8__
 
-uint8_t * memory;
+extern uint16_t stack[];
+extern uint8_t display[];
+extern uint8_t memory[];
+extern uint8_t regs[];
+
+extern int16_t delay_timer;
+extern int16_t sound_timer;
+extern int8_t stack_pointer;
+extern uint16_t addr;
+extern uint16_t program_counter;
 
 #endif
