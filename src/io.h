a93e9151f5df2eaf9b43be58306f8c133465ca13
James McClain <jmcclain@daystrom-data-concepts.com>
2022-11-23 07:47:56 -0500

    LLVM-based code generation (#5)
    * "Hello World" LLVM JIT example
    
    * LLVM-based code generation
    
    * Remove "Hello World" LLVM example
    
    * Fix load_on_key instruction
    
    * Per-module optimizations
    
    * Continue through "skip" instructions
    
    * Further extend traces
    
    * Extend traces through jump instructions

--- src/io.h
+++ src/io.h
@@ -3,6 +3,10 @@
 
 #define DISPLAY_SIZE (128*64)
 
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
 extern uint8_t display[];
 extern int width;
 extern int height;
@@ -14,5 +18,9 @@ int draw_io(int x, int y, int n, uint8_t * mem);
 uint32_t read_keys_io();
 void clearscreen_io();
 void refresh_io();
+
+#if defined(__cplusplus)
+}
+#endif
 
 #endif
